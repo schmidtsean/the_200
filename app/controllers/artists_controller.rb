0568622a34355d7ddb1ac193324a6dc4e1f81581
Jared Fitts <jfitts@gmail.com>
2021-03-08 13:58:13 -0700

    updated artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -33,15 +33,32 @@ class ArtistsController < ApplicationController
   def destroy
     @artist.destroy
     redirect_to playlist_artists_path(@playlist)
+  else
+    render component: 'ArtistNew', props: { playlist: @playlist, artist: @artist}
   end
-  private
-  def artist_params
-    params.require(:artist).permit(:name, :body)
+end
+def edit
+  render component: 'ArtistEdit', props: { playlist: @playlist, artist: @artist}
+end
+def update
+  if @artist.update(artist_params)
+    redirect_to playlist_artists_path(@playlist)
+  else
+    render component: 'ArtistEdit', props: { playlist: @playlist, artist: @artist }
   end
-  def set_playlist
-    @playlist = Playlist.find(params[:playlist_id])
-  end
-  def set_artist
-    @artist = @playlist.artists.find(params[:id])
-  end
+end
+def destroy
+  @artist.destroy
+  redirect_to playlist_artists_path(@playlist)
+end
+private
+def artist_params
+  params.require(:artist).permit(:name, :body)
+end
+def set_playlist
+  @playlist = Playlist.find(params[:playlist_id])
+end
+def set_artist
+  @artist = @playlist.artists.find(params[:id])
+end
 end
