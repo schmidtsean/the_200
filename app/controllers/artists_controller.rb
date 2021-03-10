60d5fce4216331acaf01cddbd316d9738ee10c48
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-09 20:37:32 -0700

    removed extra methods

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -38,39 +38,6 @@ class ArtistsController < ApplicationController
     end
   end
 
-  def show
-    render component: 'Aritist', props: { playlist: @playlist, artist: @artist}
-  end
-
-  def new
-    @artist = @playlist.artists.new
-    render component: 'ArtistNew', props: { playlist: @playlist, artist: @artist }
-  end
-
-  def create
-    @artist = @playlist.artists.new(artist_params)
-    if @artist.save
-      redirect_to playlist_artists_path(@playlist)
-    else
-      render component: 'ArtistNew', props: { playlist: @playlist, artist: @artist}
-    end
-  end
-  def edit
-    render component: 'ArtistEdit', props: { playlist: @playlist, artist: @artist}
-  end
-
-  def update
-    if @artist.update(artist_params)
-      redirect_to playlist_artists_path(@playlist)
-    else
-      render component: 'ArtistEdit', props: { playlist: @playlist, artist: @artist }
-    end
-  end
-  def destroy
-    @artist.destroy
-    redirect_to playlist_artists_path(@playlist)
-  end
-
   private
   def artist_params
     params.require(:artist).permit(:name, :body)
